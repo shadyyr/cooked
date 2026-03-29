8c0a6a048f8e7a690136cccfb60e80c580e3a1d7
Natalie Reese <natrse@icloud.com>
2026-03-29 02:01:26 -0400

    Update fallbacks.py to include image urls

--- backend/fallbacks.py
+++ backend/fallbacks.py
@@ -3,86 +3,102 @@ FALLBACK_RECIPES = {
     'scrambled eggs': {
         'egg': '2',
         'milk': '0.25 cup',
-        'butter': '1 tbsp'
+        'butter': '1 tbsp',
+        'image_url': 'https://www.themealdb.com/images/media/meals/1550441957.jpg'
     },
     'grilled cheese': {
         'bread': '2 slice',
         'cheese': '2 slice',
-        'butter': '1 tbsp'
+        'butter': '1 tbsp',
+        'image_url': 'https://www.themealdb.com/images/media/meals/puuker1552547438.jpg'
     },
     'pasta tomato': {
         'pasta': '200 g',
         'canned tomato': '1 can',
         'olive oil': '1 tbsp',
-        'garlic': '1 clove'
+        'garlic': '1 clove',
+        'image_url': 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg'
     },
     'simple salad': {
         'lettuce': '1 piece',
         'tomato': '1 piece',
-        'olive oil': '1 tbsp'
+        'olive oil': '1 tbsp',
+        'image_url': 'https://www.themealdb.com/images/media/meals/llcoza1604833577.jpg'
     },
-
     'omelette': {
         'egg': '3',
         'milk': '2 tbsp',
-        'butter': '1 tsp'
+        'butter': '1 tsp',
+        'image_url': 'https://www.themealdb.com/images/media/meals/surjysw1632509859.jpg'
     },
     'french toast': {
         'bread': '2 slice',
         'egg': '2',
         'milk': '0.25 cup',
-        'butter': '1 tbsp'
+        'butter': '1 tbsp',
+        'image_url': 'https://www.themealdb.com/images/media/meals/wvpsuc1468227081.jpg'
     },
     'garlic bread': {
         'bread': '4 slice',
         'butter': '2 tbsp',
-        'garlic': '2 clove'
+        'garlic': '2 clove',
+        'image_url': 'https://www.themealdb.com/images/media/meals/1550189733.jpg'
     },
     'cheese omelette': {
         'egg': '2',
         'cheese': '1 slice',
-        'butter': '1 tsp'
+        'butter': '1 tsp',
+        'image_url': 'https://www.themealdb.com/images/media/meals/surjysw1632509859.jpg'
     },
     'buttered pasta': {
         'pasta': '150 g',
         'butter': '1 tbsp',
-        'salt': '1 tsp'
+        'salt': '1 tsp',
+        'image_url': 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg'
     },
     'aglio e olio': {
         'pasta': '200 g',
         'olive oil': '2 tbsp',
-        'garlic': '3 clove'
+        'garlic': '3 clove',
+        'image_url': 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg'
     },
     'tomato pasta': {
         'pasta': '200 g',
         'tomato': '2 piece',
         'olive oil': '1 tbsp',
-        'garlic': '1 clove'
+        'garlic': '1 clove',
+        'image_url': 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg'
     },
     'cheese quesadilla': {
         'cheese': '2 slice',
-        'butter': '1 tbsp'
+        'butter': '1 tbsp',
+        'image_url': 'https://www.themealdb.com/images/media/meals/rmoqwq1548838103.jpg'
     },
     'pan fried potatoes': {
         'potato': '2 piece',
         'olive oil': '1 tbsp',
-        'salt': '1 tsp'
+        'salt': '1 tsp',
+        'image_url': 'https://www.themealdb.com/images/media/meals/5vyten1511384988.jpg'
     },
     'roasted potatoes': {
         'potato': '3 piece',
-        'olive oil': '1 tbsp'
+        'olive oil': '1 tbsp',
+        'image_url': 'https://www.themealdb.com/images/media/meals/5vyten1511384988.jpg'
     },
     'sauteed mushrooms': {
         'mushroom': '200 g',
         'butter': '1 tbsp',
-        'garlic': '1 clove'
+        'garlic': '1 clove',
+        'image_url': 'https://www.themealdb.com/images/media/meals/9f5629421beb16c36bfc514e94a51cf3.jpg'
     },
     'fruit bowl': {
         'apple': '1 piece',
-        'banana': '1 piece'
+        'banana': '1 piece',
+        'image_url': 'https://www.themealdb.com/images/media/meals/n1iypqz1632383930.jpg'
     },
     'strawberry banana bowl': {
         'strawberry': '1 cup',
-        'banana': '1 piece'
+        'banana': '1 piece',
+        'image_url': 'https://www.themealdb.com/images/media/meals/n1iypqz1632383930.jpg'
     }
 }
